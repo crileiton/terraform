a742f60d9d02f9322985a85ab1d5a6fd9d093321
Cristian Leiton Valencia <cristianleitonvalencia@gmail.com>
2024-11-29 17:49:37 -0500

    ADD - Practice - Subnets

--- practice_7/variables.tf
+++ practice_7/variables.tf
@@ -3,12 +3,17 @@ variable "virginia_cidr" {
   type        = string
 }
 
-variable "public_subnet" {
-  description = "CIDR block for the public subnet"
-  type        = string
-}
+# variable "public_subnet" {
+#   description = "CIDR block for the public subnet"
+#   type        = string
+# }
 
-variable "private_subnet" {
-  description = "CIDR block for the private subnet"
-  type        = string
+# variable "private_subnet" {
+#   description = "CIDR block for the private subnet"
+#   type        = string
+# }
+
+variable "subnets" {
+  description = "List of subnets"
+  type        = list(string)
 }
