47877869f9fc62295c8480bbde299d395d07bb4b
Cristian Leiton Valencia <cristianleitonvalencia@gmail.com>
2024-11-29 13:36:54 -0500

    ADD - Practice - variables 2 - files and env

--- practice_6/vpc.tf
+++ practice_6/vpc.tf
@@ -16,11 +16,3 @@ resource "aws_vpc" "vpc_ohio" {
   }
   provider = aws.ohio
 }
-
-variable "virginia_cidr" {
-  default = "10.10.0.0/16"
-}
-
-variable "ohio_cidr" {
-  default = "10.20.0.0/16"
-}
