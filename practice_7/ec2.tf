eff9beceee97cd0cc530df259478b90600800ddb
Cristian Leiton Valencia <cristianleitonvalencia@gmail.com>
2024-12-02 23:32:23 -0500

    ADD - Practice - Terraform lifecycles

--- practice_7/ec2.tf
+++ practice_7/ec2.tf
@@ -3,4 +3,7 @@ resource "aws_instance" "public_instance" {
   instance_type = "t2.micro"
   subnet_id     = aws_subnet.public_subnet.id
   key_name      = data.aws_key_pair.key.key_name
+  # lifecycle {
+  #   replace_triggered_by = [aws_subnet.private_subnet]
+  # }
 }
