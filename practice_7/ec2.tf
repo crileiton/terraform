4b929e57b84e583f5f12ff03f6bdcb5adf4c7f2d
Cristian Leiton Valencia <cristianleitonvalencia@gmail.com>
2024-12-03 11:48:11 -0500

    ADD - Practice - Provisioners

--- practice_7/ec2.tf
+++ practice_7/ec2.tf
@@ -1,11 +1,35 @@
 resource "aws_instance" "public_instance" {
-  ami           = "ami-0453ec754f44f9a4a"
-  instance_type = "t2.micro"
+  ami           = var.ec2_specs.ami
+  instance_type = var.ec2_specs.instance_type
   subnet_id     = aws_subnet.public_subnet.id
   key_name      = data.aws_key_pair.key.key_name
+  user_data     = file("scripts/userdata.sh")
   # lifecycle {
   #   replace_triggered_by = [aws_subnet.private_subnet]
   # }
 
   vpc_security_group_ids = [aws_security_group.sg_public_instance.id]
+
+  provisioner "local-exec" {
+    command = "echo ${aws_instance.public_instance.public_ip} >> public_ip.txt"
+  }
+
+  provisioner "local-exec" {
+    when    = destroy
+    command = "echo instancia destruida >> public_ip.txt"
+  }
+
+  # provisioner "remote-exec" {
+  #   inline = [
+  #     "echo 'Hello, World' > ~/hello.txt",
+  #   ]
+
+  #   connection {
+  #     type        = "ssh"
+  #     user        = "ec2-user"
+  #     private_key = file(var.private_key_path)
+  #     host        = self.public_ip
+  #   }
+
+  # }
 }
--- practice_7/variables.tf
+++ practice_7/variables.tf
@@ -27,3 +27,13 @@ variable "sg_ingress_cidr" {
   description = "CIDR block for the ingress rule"
   type        = string
 }
+
+variable "private_key_path" {
+  description = "Path to the private key"
+  type        = string
+}
+
+variable "ec2_specs" {
+  description = "Parameters of instance"
+  type        = map(string)
+}
