001329d72e7f85a819ba7d4ee86bad69f23e98d9
Cristian Leiton Valencia <cristianleitonvalencia@gmail.com>
2024-12-04 15:11:45 -0500

    ADD - Practice - Locals 2

--- practice_8/providers.tf
+++ practice_8/providers.tf
@@ -4,6 +4,10 @@ terraform {
       source  = "hashicorp/aws"
       version = "~> 5.0, <5.78.0, !=5.78.0"
     }
+    random = {
+      source  = "hashicorp/random"
+      version = "3.6.3"
+    }
   }
   required_version = "1.10.0"
 }
---
+++ practice_8/s3.tf
@@ -0,0 +1,3 @@
+resource "aws_s3_bucket" "cerberus-bucket" {
+  bucket = local.s3-sufix
+}
